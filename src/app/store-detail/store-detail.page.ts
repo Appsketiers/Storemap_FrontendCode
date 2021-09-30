8eb64bb878cbaa3faf87de31eb24fd763f5a7ceb
Aman5537 <aman.jain@ninehertzindia.com>
2021-09-30 17:37:39 +0530

    shop cat.

--- src/app/store-detail/store-detail.page.ts
+++ src/app/store-detail/store-detail.page.ts
@@ -17,6 +17,8 @@ store_image:any;
 store_distance:any;
 store_address:any;
 matched_products:any=[];
+store_category: any=[];
+store_category_product: any=[];
 image_url = environment.image_baseurl;
   constructor(private router: Router,
     private helper: HelperService,
@@ -26,7 +28,7 @@ image_url = environment.image_baseurl;
     this.route.queryParams.subscribe((params) => {
       this.list_id = params['id'];
       this.store_id=params['store_id'];
-      console.log(this.list_id);
+      console.log(this.list_id, this.store_id);
 
       this.helper.get_location((data) => {
         this.lat = data.coords.latitude;
@@ -55,7 +57,9 @@ this.store_image=res.data.store.store_images[0];
 this.store_distance=res.data.store.distance;
 this.matched_products=res.data.matched_products;
 this.store_address=res.data.store.address;
+this.store_category=res.data.store_category;
 console.log(this.matched_products);
+console.log('store category---', this.store_category);
       });
     });
   }
@@ -69,5 +73,19 @@ console.log(this.matched_products);
     };
 
     this.router.navigate(['/shop'], navigationExtras);
+  }
+
+  open_category(i, id) {
+      this.store_category[i].open=!this.store_category[i].open;
+if(this.store_category[i].open){
+  this.helper.getByKeynew('storetoken', (res) => {
+    let body: any = { token: res, shopping_list_id: this.list_id, store_id: this.store_id, category_id:id};
+    this.helper.postMethod('store-category-product', body, (res) => {
+      this.store_category_product=res.data;
+      console.log(this.store_category_product);
+    });
+  });  
+}
+       
   }
 }
